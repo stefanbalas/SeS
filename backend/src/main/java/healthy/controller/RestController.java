2b88afc4c573355be7a4f2302123e9f778748e53
razvancornita <cornita_razvan@yahoo.co>
2020-04-24 12:02:40 +0300

    - saved user to database

--- backend/src/main/java/healthy/controller/RestController.java
+++ backend/src/main/java/healthy/controller/RestController.java
@@ -3,6 +3,7 @@ package healthy.controller;
 import healthy.model.FormModel;
 import healthy.service.FormService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,12 @@ public class RestController {
 
     @PostMapping(value = "/submitForm")
     public ResponseEntity<String> submitForm(@RequestBody FormModel formModel) {
-        return ResponseEntity.ok().body("Received request. Yay! :)");
+        try {
+            formService.validateModel(formModel);
+            formService.saveUserToDatabase(formModel);
+            return ResponseEntity.ok().body("Saved user to database. Yay! :)");
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
     }
 }
